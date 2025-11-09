00afc673a4f0987d422a8c721190dc5d126d1316
OmarInCS <omarincs@gmail.com>
2025-11-09 21:09:54 +0300

    DB First & Code First

---
+++ Aspnet20251109/EFCore/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EFCore.HrModels;
+
+Console.WriteLine("Hello, World!");
+
+var hrDB = new HrContext();
+
+var numOfDepartments = hrDB.Departments.Count();
+
+Console.WriteLine($"# Departments: {numOfDepartments}");
+
+//Microsoft.EntityFrameworkCore.SqlServer
+//Microsoft.EntityFrameworkCore.Tools
