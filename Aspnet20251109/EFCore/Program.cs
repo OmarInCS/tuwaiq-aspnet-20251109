150299caada26923aa48077e7ce449b9e95553ca
OmarInCS <omarincs@gmail.com>
2025-11-11 18:49:49 +0300

    Day 3 - CRUD EFCore

--- Aspnet20251109/EFCore/Program.cs
+++ Aspnet20251109/EFCore/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EFCore.ClinicModels;
 using EFCore.HrModels;
+using Microsoft.EntityFrameworkCore;
 
 Console.WriteLine("Hello, World!");
 
@@ -11,3 +13,63 @@ Console.WriteLine($"# Departments: {numOfDepartments}");
 
 //Microsoft.EntityFrameworkCore.SqlServer
 //Microsoft.EntityFrameworkCore.Tools
+
+// LINQ + CRUD
+
+var clinicDB = new ClinicContext();
+
+// --------------- Create -----------------
+//var p1 = new Patient {
+//    FullName = "Wael",
+//    Email = "w@gmail.com",
+//    NationalId = "1234567890",
+//    PhoneNumber = "1234567890"
+//};
+
+//clinicDB.Patients.Add(p1);
+//clinicDB.SaveChanges();
+
+//var p2 = new Patient {
+//    FullName = "Saad",
+//    Email = "s@gmail.com",
+//    NationalId = "1234567890",
+//    PhoneNumber = "1234567890",
+//    DateOfBirth = new DateOnly(2020, 10, 10)
+//};
+
+//clinicDB.Patients.Add(p2);
+//clinicDB.SaveChanges();
+
+// ------------- Update ------------------
+//var p1 = clinicDB.Patients.Find(1);
+//p1.DateOfBirth = new DateOnly(2000, 01, 01);
+//clinicDB.SaveChanges();
+
+
+//var p2 = clinicDB.Patients.FirstOrDefault(p => p.FullName == "Saad");
+//if (p2 != null) {
+//    p2.FullName = "Saad Saad";
+//    clinicDB.SaveChanges();
+//}
+
+
+//var patients = clinicDB.Patients.Where(p => p.DateOfBirth >= new DateOnly(2000, 01, 01)).ToList();
+//foreach (var p in patients)
+//{
+//    p.DateOfBirth = new DateOnly(2010, 5, 5);
+//}
+
+//clinicDB.SaveChanges();
+
+
+//clinicDB.Patients
+//.Where(p => p.FullName == "Saad Saad")
+//.ExecuteUpdate(p => p.SetProperty(p => p.Email, "ssssss@gmail.com"));
+
+
+// ------------- Delete ---------------------
+//var p1 = clinicDB.Patients.Find(2);
+//clinicDB.Patients.Remove(p1);
+//clinicDB.SaveChanges();
+
+
--- Aspnet20251109/EFCore/obj/Debug/net8.0/EFCore.AssemblyInfo.cs
+++ Aspnet20251109/EFCore/obj/Debug/net8.0/EFCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19266329333db13781f23c9ff6e22f1c523a6d00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a39dbad133ae45bfadbdc9fde829952fe1b7f6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
